6ba80548b0e7cbc7d3e1957bfbae32b794aaf733
Luka <luka.luka.luka93@gmail.com>
2025-01-09 14:43:47 -0800

    adding an event listener to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {CORE_CONCEPTS} from "./data.js";
 import Header from "./components//Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 
 
@@ -19,6 +20,14 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[2]} />
           <CoreConcept {...CORE_CONCEPTS[3]} />
         </ul>
+      </section>
+      <section id="examples">
+        <menu>
+          <TabButton>Components</TabButton>
+          <TabButton>JSX</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>State</TabButton>
+        </menu>
       </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton({children}){
+    function handleClick () {
+        console.log('Button clicked');
+    }
+
+    return <li><button onClick={handleClick}>{children}</button></li>
+}
